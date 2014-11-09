93020a77b20304027282d7e2e9cf233be3bafef9
root <root@localhost.localdomain>
2014-11-09 05:58:44 -0800

    add handlesome

---
+++ handle.cpp
@@ -0,0 +1,20 @@
+#include "handle.h"
+using namespace std;
+
+struct Handle::Cheshire {
+	int i;
+};
+
+void Handle::initialize() {
+	smile = new Cheshire;
+	smile->i = 0;
+}
+void Handle::cleanup() {
+	delete smile;
+}
+int Handle::read() {
+	return smile->i;
+}
+void Handle::change(int x) {
+	smile->i = x;
+}
