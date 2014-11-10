cde57ab4f74d06ff25c79696801cb09f22488a7c
root <root@localhost.localdomain>
2014-11-10 03:57:37 -0800

    add stash2 files

---
+++ stash2.h
@@ -0,0 +1,17 @@
+#ifndef STASH2_H
+#define STASH2_H
+
+class Stash {
+	int size;
+	int quantity;
+	int next;
+	unsigned char * storage;
+	void inflate(int increase);
+public:
+	Stash(int size);
+	~Stash();
+	int add(void * element);
+	void * fetch(int index);
+	int count();
+};
+#endif//STASH2_H
